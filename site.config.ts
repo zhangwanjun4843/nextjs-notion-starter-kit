e0d3a67aaf269c634261fa3e6298208f29cf7e3f
Jacquelin Cassens <83950302+zhangwanjun4843@users.noreply.github.com>
2024-12-14 16:17:38 +0800

    Update site.config.ts
    feat:personnal commit

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '万有尹力',
+  domain: 'blog.zhangwanjun.online',
+  author: 'ZWJ',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '专注技术、持续学习',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
